ce5c89dc1b87ca1b914b6b5902cefd5e6e4fb33b
raman tehlan <ramantehlan@gmail.com>
2018-08-08 17:40:13 +0530

    Build

---
+++ src/index.d.ts
@@ -0,0 +1,21 @@
+/*******************************
+*  This is to log the activites of a web application
+*
+*  Date of Creation: 01-08-2018
+*  Creator: Raman Tehlan
+*******************************/
+export declare class JTLog {
+    allowLog: boolean;
+    pageName: string;
+    default: string;
+    table: string;
+    warn: string;
+    info: string;
+    start: string;
+    call: string;
+    head: string;
+    setAllowLog(permission: boolean): void;
+    clear(): void;
+    log(tag: string, msg: any, style?: string): void;
+    constructor();
+}
