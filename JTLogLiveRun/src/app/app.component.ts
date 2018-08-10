ccd2c004793135ddd0574e04aa6002fa7933fa92
raman tehlan <ramantehlan@gmail.com>
2018-08-10 15:31:39 +0530

    LiveRun for Testing added

---
+++ JTLogLiveRun/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { JTLog } from "../../../index";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Testing';
+
+  constructor(J:JTLog){
+  	J.log("sdf","testing");
+  }
+
+}
