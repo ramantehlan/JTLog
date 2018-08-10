3c7ffd10269a1f703a84a366341ba8a0dc0d7810
raman tehlan <ramantehlan@gmail.com>
2018-08-10 16:09:55 +0530

    Minor Chances

--- README.md
+++ README.md
@@ -76,7 +76,8 @@ No | Tag | Description3
 4 | info | Print Information 
 5 | start | Print Starting Heading
 6 | call | Print when a function is called
-7 | file | Print when a file is opened 
+7 | file | Print when a file is opened, also chances the fileName.
+8 | error | Print a error 
 
 ## Test 
 ```sh
--- index.ts
+++ index.ts
@@ -116,7 +116,8 @@ export class JTLog {
 						start.push({	
 										tag: "startBold" , 
 										msg: "App Name: " + msg + "\n" +
-											 "App Starting Time: " + this.config["appStartTime"]
+											 "App Starting Time: " + this.config["appStartTime"] + "\n" +
+											 "JTLog Version: " + "1.3.3"  
 										,
 										style:  ""
 									});
